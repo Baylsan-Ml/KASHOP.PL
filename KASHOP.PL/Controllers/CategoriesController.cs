c8c46d3954188d57f5e1f8e2a3ef61cc62bfcdcd
Baylsan-Ml <bailsanmletat99@gmail.com>
2026-07-02 19:21:39 +0300

    getAll

---
+++ KASHOP.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using KASHOP.DAL;
+using KASHOP.DAL.Data;
+using KASHOP.DAL.DTO;
+using KASHOP.DAL.Models;
+using KASHOP.PL.Resources;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+
+namespace KASHOP.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IStringLocalizer<SharedResources> _localizer;
+        public CategoriesController(ApplicationDbContext context, IStringLocalizer<SharedResources> localizer)
+        {
+            _context = context;
+            _localizer = localizer;
+        }
+       
+        [HttpGet("")]
+        public ActionResult Index()
+        {
+            var categories = _context.Categories.Include(c => c.Translations).ToList();
+            var response = categories.Adapt<List<CategoryResponse>>();
+            return Ok(new { _localizer["Success"].Value, response });
+        }
+        [HttpPost("")]
+        public IActionResult Create(CategoryRequest request)
+        {
+            var category= request.Adapt<Category>();
+            _context.Add(category);
+            _context.SaveChanges();
+
+            return Ok(request);
+        }
+    }
+
+}
--- KASHOP.PL/Program.cs
+++ KASHOP.PL/Program.cs
@@ -1,6 +1,11 @@
 
-using KASHOP.DAL;
+using KASHOP.BLL;
+using KASHOP.DAL.Data;
+using KASHOP.DAL.Repository;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Globalization;
 
 namespace KASHOP.PL
 {
@@ -15,12 +20,31 @@ namespace KASHOP.PL
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
-
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             {
-                options.UseSqlServer("Data Source=.;Database=KASHOP;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            builder.Services.AddLocalization(options => options.ResourcesPath = ""); //Language support
+
+            const string defaultCulture = "en";
+            var supportedCultures = new[]
+            {
+                 new CultureInfo(defaultCulture),
+                 new CultureInfo("ar")
+            };
+            builder.Services.Configure<RequestLocalizationOptions>(options => {
+                options.DefaultRequestCulture = new RequestCulture(defaultCulture);
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders.Clear();
+                options.RequestCultureProviders.Add(new AcceptLanguageHeaderRequestCultureProvider());
+            });
+
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
             var app = builder.Build();
+            app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
